980343caff6905789f17a57c9be3a94e29cc3b88
Nathan Wong <nathan2wong@gmail.com>
2016-07-19 12:40:34 -0700

    new

--- script/subtract.js
+++ script/subtract.js
@@ -1,38 +1,38 @@
 var sum = 0;
-    var count = 0;
-function timr(){
-    console.log(document.getElementById("count"))
-    
-    if(count < document.getElementById("count").value)
-    {setTimeout(Adding, document.getElementById("num").value * 1000);
-    count++;
+    var count2 = 0;
+function timr2() {
+    console.log(document.getElementById("count2"))
+
+    if(count2 < document.getElementById("count2").value)
+    {setTimeout(Subtract, document.getElementById("num2").value * 1000);
+    count2++;
     }
     else{
-        count = 0;
+        count2 = 0;
     }
 }
-function Adding() {
-    var x = document.getElementById("demo")
-    var y = document.getElementById("digits").value
+function Subtract() {
+    var x = document.getElementById("demo2")
+    var y = document.getElementById("digits2").value
     var pop = ((Math.round(Math.random())*2)-1)*(Math.floor(Math.random()*(Math.pow(10,y)-Math.pow(10,(y-1)))+Math.pow(10,(y-1))));
     sum += pop;
     x.innerHTML = pop;
     //document.getElementById("answer").innerHTML = "";
-    timr()
+    timr2()
 }
-function Hiding() {
+function Hiding2() {
     //if (document.getElementById("answer").innerHTML == "") {
         alert(sum)
          //document.getElementById("answer").innerHTML == sum;
-    
+
    // }
    // else if (document.getElementById("answer").innerHTML == sum) {
   //         document.getElementById("answer").innerHTML = "";
   //      console.log("bad")
 //    }
 }
-function Check() {
-    if (document.getElementById("answercheck").value == sum) {
+function Check2() {
+    if (document.getElementById("answercheck2").value == sum) {
         confirm("congrats you got it right!");
         console.log("ok");
     }
